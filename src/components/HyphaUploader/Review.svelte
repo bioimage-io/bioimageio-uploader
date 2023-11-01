4b07efb80f03f70275ab106b52bda43741265ce1
Jeremy Metz <metz.jp@gmail.com>
2023-11-01 15:27:56 +0100

    Added error catching on upload, fixes #7

--- src/components/HyphaUploader/Review.svelte
+++ src/components/HyphaUploader/Review.svelte
@@ -11,6 +11,7 @@
     //import yaml from "js-yaml";
     import JSONTree from 'svelte-json-tree';
     let all_done = false;
+    let error;
     let storage;
     let storage_info;
     let uploading = false;
@@ -23,21 +24,29 @@
 
     async function upload_file_to_hypha(url, file){
         const filename = file.name; 
-        let response = await fetch(
-            url, 
-            {
-                method:"PUT", 
-                body:file, 
-                credentials:"include", 
-                mode: "cors",
-                headers:upload_headers
-            });
-        console.log("Upload result:", await response.text());
-        let presigned_url = await storage.generate_presigned_url(
-            storage_info["bucket"], 
-            storage_info["prefix"] + filename
-        )
-        return presigned_url;
+        try{
+            let response = await fetch(
+                url, 
+                {
+                    method:"PUT", 
+                    body:file, 
+                    credentials:"include", 
+                    mode: "cors",
+                    headers:upload_headers
+                });
+            console.log("Upload result:", await response.text());
+            let presigned_url = await storage.generate_presigned_url(
+                storage_info["bucket"], 
+                storage_info["prefix"] + filename
+            )
+            return presigned_url;
+        }catch(err){
+            console.error("Upload failed!");
+            console.error(err);
+            error = err.message;
+        }
+        return null;
+    
     }
 
     async function publish(){
@@ -60,10 +69,15 @@
         let filename = zip_package.filename;
         let url = `${server_url}/${workspace}/files/${filename}`;
         presigned_url = await upload_file_to_hypha(url, zip_package);
+
         //for(const file of files){
             // let presigned_url = await upload_file_to_hypha(url, file); 
         //}
         uploading = false;
+        console.log(presigned_url);
+        if(!presigned_url){
+            return;
+        }
 
         all_done_here = true;
         await new Promise(r => setTimeout(r, 2000));
@@ -77,17 +91,21 @@
 </script>
 
 <h2>Review & Publish</h2>
-<p>Please review your submission carefully, then press Publish</p>
-
-<JSONTree value={rdf}/>
-
-<br>
 
 {#if uploading}
-    Uploading 
+    <p>Uploading</p> 
     <progress class="progress is-small is-primary" max="100">15%</progress>
 {:else if all_done}
-    All done!
+    <p>All done!</p>
+{:else if error}
+    <p>Oops! Something went wrong 😟</p>
+    <p>Please review the error, and try again. If the issue persists, please contact support</p>
+    <code>{error}</code>
 {:else}
+    <p>Please review your submission carefully, then press Publish</p>
+
+    <JSONTree value={rdf}/>
+
+    <br>
     <button class="button is-primary" on:click={publish}>Publish</button>
 {/if}
