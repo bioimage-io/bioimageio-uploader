2fdc2df84a93d0838f541a0baa3adfc4ab152991
Jeremy Metz <metz.jp@gmail.com>
2023-12-05 14:49:02 +0100

    Updated vite config to remove iso import

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { isoImport } from 'vite-plugin-iso-import';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), isoImport()],
+  plugins: [svelte()]
 })
