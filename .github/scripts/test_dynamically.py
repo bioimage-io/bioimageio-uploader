29db72026f89dfb093212980d203f45ad384b3cc
fynnbe <thefynnbe@gmail.com>
2024-02-07 14:09:38 +0100

    remove unused imports

--- .github/scripts/test_dynamically.py
+++ .github/scripts/test_dynamically.py
@@ -1,4 +1,3 @@
-import json
 import traceback
 from functools import partialmethod
 from pathlib import Path
--- .github/scripts/validate_format.py
+++ .github/scripts/validate_format.py
@@ -1,6 +1,5 @@
 import json
 import os
-import shutil
 import uuid
 import warnings
 from functools import partialmethod
