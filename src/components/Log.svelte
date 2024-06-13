c39a86a72ee4fd6e4afb507831327740b5005c9b
Wei Ouyang <oeway007@gmail.com>
2024-06-13 15:08:36 +0200

    Display logs

--- src/components/Log.svelte
+++ src/components/Log.svelte
@@ -2,12 +2,16 @@
     import { onDestroy } from 'svelte';
     import { get_json } from '../lib/utils';
     import { RESOURCE_URL } from '../lib/config';
-
+    import github from '../../static/github.svg';
     import ErrorBox from './ErrorBox.svelte';
 
     export let resource_id="";
-    export let version = "";
-
+    function toTitleCase(str) {
+        return str.replace(/_/g, ' ').replace(/\w\S*/g, function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        });
+    }
+    
     let error = "";
     let error_object: Error;
     let timeout_id : ReturnType<typeof setTimeout>;
@@ -22,10 +26,10 @@
     });
 
     async function poll(){
-        if(resource_id && version){ 
+        if(resource_id){ 
             try{
-                log = await get_json(`${RESOURCE_URL}/${resource_id}/${version}/log.json`);
-                console.debug(log);
+                log = await get_json(`${RESOURCE_URL}/${resource_id}/draft/log.json`);
+                console.debug("======log=======>", log);
                 error = "";
             }catch(err){
                 console.error("Error polling logs:");
@@ -50,14 +54,25 @@
 {/each}
 
 {#each Object.entries(log) as [category, logs_messages]}
+<h3>{toTitleCase(category)}</h3>
 <ul>
-    <h3>{category}</h3>
+
     {#each logs_messages as message}
     <li>
-        <p> <span title="{message.timestamp}" >{message.log.message || ""}</span>
-
+        <p>
+            <span title="{message.timestamp}" >{message.log.message || ""}
+                {#if message.log.details && message.log.details.status === 'passed'}
+                <span>✔️</span>
+                {:else}
+                    <span>❌</span>
+                {/if}
+                {#if message.log.run_url}
+                <a href={message.log.run_url} target="_blank"><img src="{github}" alt="github icon">Github CI Logs</a>
+                {/if}
+            </span>
         </p>
         {#if message.log.details}
+            
             <details>
                 <code>{message.timestamp}</code>
                 <pre>{JSON.stringify(message.log.details, null, "\t")}</pre>
