368f281b50a0083ee45a6ddd6f6c0cc04bdc4edb
Wei Ouyang <oeway007@gmail.com>
2024-06-14 12:03:51 +0200

    Improve generate_names

--- src/lib/generate_name.ts
+++ src/lib/generate_name.ts
@@ -1,28 +1,12 @@
 import { COLLECTION_URL_PUBLISHED, COLLECTION_CONFIG_URL } from './config'; 
 
 
-async function get_taken_names(): Promise<Array<string>>{
-    // Url of collection
-    const url = COLLECTION_URL_PUBLISHED;
-    const obj = await (await fetch(url)).json();
-    return obj.collection.map((item : {nickname: string})=>item.nickname).filter((item : string)=>item !== undefined);
-}
-
-
-async function get_id_parts(type: string): Promise<Array<string>>{
-    const url = COLLECTION_CONFIG_URL;
-    const config = await (await fetch(url)).json();
-    
-    return config["id_parts"][type]
-}
-
-
 function sample(array: Array<any>){
     return array[Math.floor(Math.random() * array.length)];
 }
 
 
-function generate_name(starts:Array<string>, animals:Array<Array<string>>): {id:string, emoji:string}{
+function sample_name(starts:Array<string>, animals:Array<Array<string>>): {id:string, emoji:string}{
     const animal = sample(animals);
     return {id: `${sample(starts)}-${animal[0]}`, emoji: animal[1]};
 }
@@ -30,13 +14,14 @@ function generate_name(starts:Array<string>, animals:Array<Array<string>>): {id:
 
 export default async (type): Promise<{id:string, emoji:string}> => {
     console.log("Generating nickname from allowed lists...");
-    const [id_parts, taken_names] = await Promise.all([
-        get_id_parts(type),
-        get_taken_names()]);
+    const config = await (await fetch(COLLECTION_CONFIG_URL)).json();
+    const id_parts = config["id_parts"][type]
+    const obj = await (await fetch(COLLECTION_URL_PUBLISHED)).json();
+    const taken_names = obj.collection.map((item : {nickname: string})=>item.nickname).filter((item : string)=>item !== undefined);
     const values: Array<Array<string>> = Object.entries(id_parts['nouns']);
-    let name = generate_name(id_parts['adjectives'], values);
+    let name = sample_name(id_parts['adjectives'], values);
     while(name.id in taken_names){
-        name = generate_name(id_parts['adjectives'], values);
+        name = sample_name(id_parts['adjectives'], values);
     }
     return name; 
 }
